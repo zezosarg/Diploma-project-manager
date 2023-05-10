d433cd61f5b0ae04427f53391da7738d6acb5693
zezosarg <zezosarg@protonmail.com>
2023-05-10 17:44:59 +0300

    fixing issue #4

--- src/main/java/diplomaProjectManager/dao/SubjectDAO.java
+++ src/main/java/diplomaProjectManager/dao/SubjectDAO.java
@@ -14,8 +14,8 @@ public interface SubjectDAO extends JpaRepository<Subject, Integer>{
 	@Query(value="SELECT * FROM subjects JOIN users ON professor_id=user_id WHERE user_name=?1 AND subject_id NOT IN (SELECT subject_id FROM theses)", nativeQuery=true)
 	List<Subject> findByProfessorUsername(String username);
 	
-	@Query(value="SELECT * FROM subjects WHERE subject_id NOT IN (SELECT subject_id FROM theses)", nativeQuery=true)
-	List<Subject> findAvailable();
+	@Query(value="SELECT * FROM subjects WHERE subject_id NOT IN (SELECT subject_id FROM theses) AND subject_id NOT IN (SELECT subject_id FROM applications TODO)", nativeQuery=true)//TODO
+	List<Subject> findAvailableUnappliedByStudentId(int id);
 	
 	Subject findById(int subjectId);
 }
